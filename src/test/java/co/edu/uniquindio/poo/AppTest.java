cb3951e6066d7b5f9895a50bdee98f411d60c9df
ecodant <edwincodant@gmail.com>
2023-10-20 19:46:16 -0500

    base proyecto

---
+++ src/test/java/co/edu/uniquindio/poo/AppTest.java
@@ -0,0 +1,18 @@
+package co.edu.uniquindio.poo;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        
+    }
+}
